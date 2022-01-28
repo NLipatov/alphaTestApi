665cb1b5fe2bcd180cabf042ce035dcddf557032
Lipatov Nikita <lipatovnn@yahoo.com>
2022-01-28 21:19:07 +0300

    new DTO, new methods for repository class

---
+++ alphaApi/DTOs/DepartmentsAndManagersByFirtmDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace alphaApi.DTOs
+{
+    public class DepartmentsAndManagersByFirtmDTO
+    {
+        public int FirmId { get; set; }
+        public string FirmTitle { get; set; }
+        public string Fullname { get; set; }
+        public int DepartmentId { get; set; }
+        public string DepartmentTitle { get; set; }
+    }
+}
